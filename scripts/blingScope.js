7d467d344412e1a13eea0a3de12265dadab1e3c6
devjanaprime <dev@primeacademy.io>
2017-06-27 09:51:52 -0500

    bling scope conversion

--- scripts/blingScope.js
+++ scripts/blingScope.js
@@ -1,9 +1,13 @@
 var myApp = angular.module( 'myApp', [] );
 
-myApp.controller( 'TestController', function(){
-  var vm=this;
+myApp.controller( 'TestController', function( $scope ){
+  $scope.things = [
+    'asdf',
+    'qwer',
+    'zxcv'
+  ]; // end things
 
-  vm.testClick = function(){
+  $scope.testClick = function(){
     console.log( 'in testClick' );
   }; // end testClick
 }); // end TestController
